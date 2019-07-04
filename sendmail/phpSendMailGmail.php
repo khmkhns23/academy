a210ac3b6a4f448960e88069e926c7b32e6f2dfb
khmkhns <khmkhns@gmail.com>
2019-07-05 00:34:49 +0700

    edit

--- sendmail/phpSendMailGmail.php
+++ sendmail/phpSendMailGmail.php
@@ -21,9 +21,9 @@
 	$mail->From = "khmkhns@gmail.com"; // "name@yourdomain.com";
 	//$mail->AddReplyTo = "support@thaicreate.com"; // Reply
 	$mail->FromName = iconv('TIS-620', 'UTF-8', 'เว็บไซต์สมาคมตระกูลแซ่');  // set from Name
-	$msg = "ยืนยันการลงทะเบียน <b>ท่านได้ทำการลงทะเบียนใช้งานเว็บไซต์ ชมรมตระกูลแซ่ <br>เพื่อให้การลงทะเบียนสมบูรณ์คุณจะต้องยืนยันการลงทะเบียน ด้วยลิงค์นี้'</b><a href='http://academyyut.herokuapp.com";
+	$msg = iconv('TIS-620', 'UTF-8','ยืนยันการลงทะเบียน <b>ท่านได้ทำการลงทะเบียนใช้งานเว็บไซต์ ชมรมตระกูลแซ่ <br>เพื่อให้การลงทะเบียนสมบูรณ์คุณจะต้องยืนยันการลงทะเบียน ด้วยลิงค์นี้</b><a href=http://academyyut.herokuapp.com');
 	$mail->Subject = iconv('TIS-620', 'UTF-8','ยืนยันการลงทะเบียน'); 
-	$mail->Body = iconv('TIS-620', 'UTF-8',$msg);
+	$mail->Body = $msg;
 
 	$mail->AddAddress("khm_s@hotmail.com", "Mr.Adisorn Boonsong"); // to Address
 
